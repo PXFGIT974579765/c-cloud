a786a0c110e8bbce1c997c0314cdb8c18adba592
peng xiongfeng <pengxiongfeng@cetcbigdata.com>
2020-03-12 00:10:05 +0800

    feat: resultVO and resultVOUtil

---
+++ product/src/main/java/com/ccloud/product/utils/ResultVOUtil.java
@@ -0,0 +1,31 @@
+package com.ccloud.product.utils;
+
+import com.ccloud.product.vo.ResultVo;
+
+/**
+ * @author ：腾云先生
+ * @date ：Created in 2020/03/12 00:08
+ * @description：最外层返回对象的包装工具
+ * @modified By：
+ */
+public class ResultVOUtil {
+
+    public static ResultVo success(Object object) {
+        ResultVo resultVo = new ResultVo();
+        resultVo.setData(object);
+        resultVo.setCode(0);
+        resultVo.setMessage("成功");
+        return resultVo;
+    }
+
+    public static ResultVo success() {
+        return success(null);
+    }
+
+    public static ResultVo error(Integer code, String msg) {
+        ResultVo resultVo = new ResultVo();
+        resultVo.setCode(code);
+        resultVo.setMessage(msg);
+        return resultVo;
+    }
+}
