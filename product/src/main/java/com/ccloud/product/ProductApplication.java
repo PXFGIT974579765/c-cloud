67514cde71c449d22c0293fee092043d32f7fed7
peng xiongfeng <pengxiongfeng@cetcbigdata.com>
2020-03-11 23:50:22 +0800

    fix: make product server enable cache

--- product/src/main/java/com/ccloud/product/ProductApplication.java
+++ product/src/main/java/com/ccloud/product/ProductApplication.java
@@ -2,10 +2,12 @@ package com.ccloud.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 public class ProductApplication {
 
     public static void main(String[] args) {
